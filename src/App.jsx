58fa276e4f19654ed0a2eec39909674546d558e1
IgorCisowski <igorcisowski2098@gmail.com>
2023-12-26 22:37:57 +0000

    feat:hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./Global.css";
 import Header from "./components/ui/header";
+import Hero from "./components/ui/hero";
 
 function App() {
   return (
     <main className="w-full max-w-[1564px] mx-auto">
       <Header />
+      <Hero />
     </main>
   );
 }
--- src/components/elements/blackButton.jsx
+++ src/components/elements/blackButton.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function BlackButton({ children }) {
   return (
     <button className="relative -z-20 px-5 py-2 bg-gradient-to-r from-cyan to-pink">
-      <div className="absolute -z-10 left-0 top-0 w-full h-full scale-95 bg-[#000000]"></div>
-      <p className="z-10 w-full">{children}</p>
+      <div className="absolute -z-10 left-[2px] top-[2px] right-[2px] bottom-[2px] bg-[#000000]"></div>
+      <p className="z-10 w-full text-xl">{children}</p>
     </button>
   );
 }
