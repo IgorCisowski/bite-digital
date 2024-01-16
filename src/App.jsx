3cff9331895adab8d43853d37c2f4be2a5335d28
IgorCisowski <igorcisowski2098@gmail.com>
2024-01-16 11:45:24 +0000

    feat: video for hero section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./Global.css";
 import Header from "./components/ui/header";
 import Footer from "./components/ui/footer";
 import heroBg from "./images/hero-bg.jpg";
+import video from "./images/video.mp4";
 import Hero from "./components/ui/hero";
 import About from "./components/ui/about";
 import BusinessSupport from "./components/ui/businessSupport";
@@ -13,8 +14,9 @@ import Try from "./components/ui/try";
 function App() {
   return (
     <div className="relative">
-      <div className="absolute -z-20 top-0 left-0 w-full">
-        <img src={heroBg} alt="hero" />
+      <div className="absolute -z-20 top-40 left-0 w-full">
+        <video src={video} autoPlay loop muted playsInline></video>
+        {/* <img src={heroBg} alt="hero" /> */}
       </div>
       <div className="wrapper px-6">
         <Header />
